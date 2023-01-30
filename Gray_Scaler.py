2a608ab6638ad4d01ea2e57cbbfce2a840804351
Halivious <73369052+Halivious@users.noreply.github.com>
2023-01-30 15:18:06 +0300

    Add files via upload

---
+++ Gray_Scaler.py
@@ -0,0 +1,23 @@
+import glob
+import os
+from PIL import Image
+
+
+dir_path = "/home/htranaliz/Desktop/All_Data/All_images/" +"*.jpg"
+filenames = glob.glob(dir_path)
+k=0
+
+for filename in filenames:
+  if k == 182797 :
+    break
+  with Image.open(filename) as im:
+     
+     k +=1
+     output_dir= "/home/htranaliz/Desktop/All_Data/All_images_Gray/"+str(k) +"*.jpg"
+     im = im.convert('L')
+     im.save(os.path.join(filename, output_dir))
+     print(output_dir)
+      
+  
+    
+    
